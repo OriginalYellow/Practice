db42ac813c70e077ca50afd2001181fc3aec59dc
OriginalYellow <originalyellow@yahoo.com>
2014-06-26 22:38:05 -0400

    Added new test

---
+++ CS101/src/intro2java/chap30/Test.java
@@ -0,0 +1,11 @@
+package intro2java.chap30;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Test {
+	public static void main(String[] args) throws UnknownHostException {
+		System.out.println(InetAddress.getAllByName("localhost").toString());
+	}
+
+}

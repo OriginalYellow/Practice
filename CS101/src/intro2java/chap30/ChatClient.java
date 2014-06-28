039685781169ae4f92ed3da7a858c51a4d45dd68
OriginalYellow <originalyellow@yahoo.com>
2014-06-28 16:21:52 -0400

    improved chatclient

--- CS101/src/intro2java/chap30/ChatClient.java
+++ CS101/src/intro2java/chap30/ChatClient.java
@@ -1,5 +1,7 @@
 package intro2java.chap30;
 
+import intro2java.chap19.Loan;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +16,7 @@ public class ChatClient extends JFrame {
 	private JTextField chatField = new JTextField(20);
 	private JTextArea area = new JTextArea(7, 25);
 	private JButton send = new JButton("Send");
+	private boolean started = false;
 
 	public ChatClient() throws UnknownHostException, IOException {
 		area.setWrapStyleWord(true);
@@ -23,17 +26,15 @@ public class ChatClient extends JFrame {
 		JPanel p1 = new JPanel(new BorderLayout());
 		JPanel p2 = new JPanel(new BorderLayout());
 		JScrollPane scroll = new JScrollPane(area);
-		
+
 		p1.add(chatField, BorderLayout.CENTER);
 		p1.add(send, BorderLayout.EAST);
 		add(p1, BorderLayout.NORTH);
-		add(area, BorderLayout.CENTER);
+		add(scroll, BorderLayout.CENTER);
 
-		/*
 		Socket socket = new Socket("10.0.0.3", 9000);
 		input = new DataInputStream(socket.getInputStream());
 		output = new DataOutputStream(socket.getOutputStream());
-		*/
 
 		send.addActionListener(new ActionListener() {
 
@@ -42,7 +43,7 @@ public class ChatClient extends JFrame {
 				String data = chatField.getText();
 				try {
 					output.writeUTF(data);
-					area.append("\nClient: " + data + "\nServer: " + input.readUTF());
+					area.append("\nClient: " + data);
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
@@ -52,13 +53,43 @@ public class ChatClient extends JFrame {
 
 	}
 
+	public void Recieve() throws IOException {
+		
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (true) {
+					try {
+
+						Thread.sleep(500);
+						String data = input.readUTF();
+						area.append("\nServer: " + data);
+						
+					} catch (EOFException e) {
+						//area.append("\nEnd of input.");
+					} catch (IOException e) {
+						e.printStackTrace();
+						break;
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						break;
+					}
+				}
+			}
+
+		});
+
+		t.run();
+	}
+
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		ChatClient frame = new ChatClient();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-
+		frame.Recieve();
 	}
 
 }
